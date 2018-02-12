1b06ea437708d2301f16966092958ed4102949b4
Miroslav Radojevic <10250153@bd.com>
2018-02-12 11:41:09 +0100

    First version of the owin based self hosting application with basic single page application and some basic web api implemented.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "http://localhost:9090"; // webserver running on port 9090
+            using (WebApp.Start<Startup>(baseUrl))
+            {
+                Console.WriteLine(baseUrl);
+                Process.Start(baseUrl); // open the page from the application
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
