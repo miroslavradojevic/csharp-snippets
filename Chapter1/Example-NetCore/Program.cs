b2dea6c6e3e41d9aaa913427aaa620cf60d59a53
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2019-10-19 19:12:03 +0200

    Added console C# project that compiles using .NET core.

---
+++ Chapter1/Example-NetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example_NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
